70e7dc31ccb982d3e2414474eb980d45b7c3ed57
rk <rakhaph07@gmail.com>
2023-10-07 13:59:08 +0700

    inital commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "nama saya rakha saya tinggal didaerah kemayoran
+        olahraga yg saya sukai adalah badminton";
+    }
+}
